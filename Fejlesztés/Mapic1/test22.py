1fdd138be18164eb6af6405b253a2e7b8c3f95fb
Majika007 <pondera77@gmail.com>
2025-10-12 21:01:10 +0200

    Open image automatically if provided as argument

--- Fejlesztés/Mapic1/test22.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ExifTags
-
-img = Image.open("111df8e2.jpeg")
-exif = img.getexif()
-for tag_id, value in exif.items():
-    tag = ExifTags.TAGS.get(tag_id, tag_id)
-    print(f"{tag:25}: {value}")
